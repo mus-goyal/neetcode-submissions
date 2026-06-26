7c3c6cef907208404c2b2d3f782a951b6758ace1
mus-goyal <55279658+mus-goyal@users.noreply.github.com>
2026-06-26 23:57:43 +0530

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const res = new Array(nums.length).fill(1);
+
+        let prefix = 1;
+
+        // Store left products
+        for (let i = 0; i < nums.length; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        let postfix = 1;
+
+        // Multiply by right products
+        for (let i = nums.length - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return res;
+    }
+}
